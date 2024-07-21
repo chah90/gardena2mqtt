52f57171a55e73606af29002de26f3e653b28055
DomoChip <domochip@hotmail.com>
2024-07-21 13:21:00 +0000

    v1.5.0 new libs version and id/secret auth

--- gardena2mqtt.py
+++ gardena2mqtt.py
@@ -154,7 +154,7 @@ def shutdown(signum=None, frame=None):
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
 
-    versionnumber = '1.0.2'
+    versionnumber = '1.5.0'
 
     logging.info(f'===== gardena2mqtt v{versionnumber} =====')
 
